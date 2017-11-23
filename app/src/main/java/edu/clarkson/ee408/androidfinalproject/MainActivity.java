ac77fcb25b393060323bc37696b622d07c980ff4
Reilly Q. Curran <reillycurran880@gmail.com>
2017-11-22 22:59:02 -0500

    pseudo coded how the app should behave, maaaaay need to ask Daqing for some help understanding how to properly implement stuffs...

--- app/src/main/java/edu/clarkson/ee408/androidfinalproject/MainActivity.java
+++ app/src/main/java/edu/clarkson/ee408/androidfinalproject/MainActivity.java
@@ -29,4 +29,37 @@ public class MainActivity extends AppCompatActivity {
         String assignedPos[] = {"adjective", "adverb", "conjunction", "interjection", "noun", "preposition", "pronoun"};
 
     }
+
+    /*
+    * page()
+    * {
+    *   int index = rand() sen.length() - 1;
+    *
+    *   textview.highlight(sen[0][index]);
+    *   while(find == 0)
+    *   {
+    *       for(int j = 0; j < 3; j++)
+    *       {
+    *           if(assignedPos[rand() assignedPos.length() - 1] == button[j].getText().toString())
+    *               find
+    *       }
+    *   }
+    * }
+    * */
+
+    /*void onClick(){
+        if(sentences.checkPos(word, button.getText().toString()))
+        {
+            setButtonColor(Color green);
+            playsound, ding
+        }
+
+        else
+        {
+            setButtonColor(Color red);
+            playsound buzzer
+        }
+
+        transition (right to left if wrong, left to right if correct);
+    }*/
 }
