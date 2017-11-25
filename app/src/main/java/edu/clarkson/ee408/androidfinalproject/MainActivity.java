6f2f531b778825f5db857042e678fe93583acdd8
Reilly Q. Curran <reillycurran880@gmail.com>
2017-11-25 15:16:30 -0500

    created game class

--- app/src/main/java/edu/clarkson/ee408/androidfinalproject/MainActivity.java
+++ app/src/main/java/edu/clarkson/ee408/androidfinalproject/MainActivity.java
@@ -30,36 +30,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*
-    * page()
-    * {
-    *   int index = rand() sen.length() - 1;
-    *
-    *   textview.highlight(sen[0][index]);
-    *   while(find == 0)
-    *   {
-    *       for(int j = 0; j < 3; j++)
-    *       {
-    *           if(assignedPos[rand() assignedPos.length() - 1] == button[j].getText().toString())
-    *               find
-    *       }
-    *   }
-    * }
-    * */
 
-    /*void onClick(){
-        if(sentences.checkPos(word, button.getText().toString()))
-        {
-            setButtonColor(Color green);
-            playsound, ding
-        }
-
-        else
-        {
-            setButtonColor(Color red);
-            playsound buzzer
-        }
-
-        transition (right to left if wrong, left to right if correct);
-    }*/
 }
---
+++ app/src/main/java/edu/clarkson/ee408/androidfinalproject/quiz.java
@@ -0,0 +1,41 @@
+package edu.clarkson.ee408.androidfinalproject;
+
+/**
+ * Created by Rancerle on 11/25/2017.
+ */
+
+public class quiz
+{
+     /*
+    * createPage()
+    * {
+    *   int index = rand() sen.length() - 1;
+    *
+    *   textview.highlight(sen[0][index]);
+    *   while(find == 0)
+    *   {
+    *       for(int j = 0; j < 3; j++)
+    *       {
+    *           if(assignedPos[rand() assignedPos.length() - 1] == button[j].getText().toString())
+    *               find
+    *       }
+    *   }
+    * }
+    * */
+
+    /*void alertPlayer(){
+        if(sentences.checkPos(word, button.getText().toString()))
+        {
+            setButtonColor(Color green);
+            playsound, ding
+        }
+
+        else
+        {
+            setButtonColor(Color red);
+            playsound buzzer
+        }
+
+        transition (right to left if wrong, left to right if correct);
+    }*/
+}
